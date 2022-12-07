89882bed0713c252c7902c37575a68d0ce270fba
D-Dursty <devondahlia@gmail.com>
2022-12-06 19:31:36 -0800

    thought model

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+const moment = require("moment/moment");
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate
+    },
+  },
+  {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+function formatDate (date) {
+
+    return moment(date).format('MMMM Do YYYY, h:mm:ss')
+}
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
