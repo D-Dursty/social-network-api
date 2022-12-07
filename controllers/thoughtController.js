f87a743715e139c343cf968c335b576b7f644091
D-Dursty <devondahlia@gmail.com>
2022-12-06 20:12:00 -0800

    C:/Users/devon/AppData/Local/Programs/Git/

---
+++ controllers/thoughtController.js
@@ -0,0 +1,130 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+
+  getThoughts(req, res) {
+    Thought.find()
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+ 
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'Thought not found' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  
+  createThought(req, res) {
+    console.log(req.body);
+    Thought.create(req.body)
+        .then(thought => {
+            User.findByIdAndUpdate(req.body.userId,
+                {
+                    $addToSet: { thoughts: thought._id }
+                },
+                { new: true })
+                .then((user) =>
+                    !user
+                        ? res.status(404).json({ message: 'No user found, your thought was logged' })
+                        : res.json(thought)
+
+                )
+                .catch((err) => res.status(500).json(err));
+        })
+},
+    
+
+updateThought(req,res){
+  Thought.findOneAndUpdate(
+    { _id: req.params.thoughtId },
+    { $set: req.body },
+    { runValidators: true, new: true }
+  )
+    .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: 'Thought not found' })
+        : res.json(thought)
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  )
+},
+
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>{
+        if (!thought) {
+          res.status(404).json({ message: 'Thoughts not found' });
+          return;
+        }
+        return User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $pull: { thoughts: req.params.Id } },
+          { new: true }
+        )
+      })
+      .then(userData => {
+        if (!userData) {
+          res.status(404).json({ message: 'Thought not found' });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch(err => res.json(err));
+  },
+
+
+  addReaction (req,res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body} },
+      {  new: true }
+    )
+    .populate({path: 'reactions', select: '-__v'})
+    .select('-__v')
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'Thought not found' })
+          : res.json(thought)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    )
+  },
+
+
+  deleteReaction({params}, res){
+    Thought.findOneAndUpdate(
+        {_id: params.thoughtId},
+        {$pull: {reactions: {_id : params.reactionId}}},
+        { new: true, runValidators: true }
+    )
+    .then(thoughtData => {
+        if (!thoughtData) {
+            res.status(404).json({ message: 'Well, that didnt work!' });
+            return;
+        }
+        res.json(thoughtData);
+    })
+    .catch(err => res.json(err));
+}
+}
+
+module.exports = thoughtController
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
-const thoughtRoutes = require('./thoughtRoutes');
-const userRoutes = require('./userRoutes');
+const {
+    createUser,
+    getUsers,
+    getSingleUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+  } = require('../../controllers/userController');
+  
+  // /api/users
+router.route('/').get(getUsers).post(createUser);
 
-router.use('/thoughts', thoughtRoutes);
-router.use('/users', userRoutes);
 
+router
+.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser)
+
+router
+.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend)
 
 module.exports = router;
